45b8eaca20d73dfa738d4bbd183637af86c7f8cf
samnoon.abrar <samnoonabrar@gmail.com>
2022-10-09 16:18:43 +0600

    added docker compose files for lamp stack

---
+++ html/index.php
@@ -0,0 +1,13 @@
+<?php
+$host = "mysql-server";
+$user = "root";
+$pass = "test";
+$db = "test";
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
